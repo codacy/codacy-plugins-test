400e5b095b1a4765dc2873d5493852f06472e020
Joao Machado <machado@codacy.com>
2016-12-19 16:31:26 +0000

    Add Dockerfile as a language

--- src/main/scala/codacy/plugins/docker/Plugin.scala
+++ src/main/scala/codacy/plugins/docker/Plugin.scala
@@ -22,7 +22,7 @@ case class PluginResult(results: Seq[Result], failedFiles: Seq[String])
 
 case object Language extends Enumeration with JsonEnumeration {
   val Javascript, Scala, CSS, PHP, C, CPP, ObjectiveC, Python, Ruby, Perl, Java, CSharp, VisualBasic, Go, Elixir, Clojure,
-  CoffeeScript, Rust, Swift, Haskell, React, Shell, TypeScript, Jade, Stylus, XML = Value
+  CoffeeScript, Rust, Swift, Haskell, React, Shell, TypeScript, Jade, Stylus, XML, Dockerfile = Value
 
   def getExtensions(value: Value): Seq[String] = {
     value match {
@@ -52,6 +52,7 @@ case object Language extends Enumeration with JsonEnumeration {
       case Jade => Seq(".jade")
       case Stylus => Seq(".styl", ".stylus")
       case XML => Seq(".xml")
+      case Dockerfile => Seq(".dockerfile")
       case _ => Seq.empty
     }
   }
