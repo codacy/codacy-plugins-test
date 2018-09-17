62da65a5003b1c0a985f651abc6e84725eb549ac
Bruno Ferreira <bruno.ferreira@codacy.com>
2018-09-17 09:13:06 +0100

    Move dependencies to a separate file.

--- build.sbt
+++ build.sbt
@@ -5,8 +5,9 @@ name := """codacy-plugins-test"""
 
 val scalaBinaryVersionNumber = "2.12"
 scalaVersion := s"$scalaBinaryVersionNumber.6"
+
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % "3.0.5",
-  "com.codacy" %% "codacy-analysis-core" % "0.1.0-alpha3.3529",
-  "commons-io" % "commons-io" % "2.4"
+  Dependencies.scalaTest,
+  Dependencies.analysisCore,
+  Dependencies.commonsIO
 )
---
+++ project/Dependencies.scala
@@ -0,0 +1,13 @@
+import sbt._
+
+object Dependencies {
+
+  private val scalatestVersion = "3.0.5"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % scalatestVersion
+
+  private val analysisCoreVersion = "0.1.0-alpha3.3529"
+  lazy val analysisCore = "com.codacy" %% "codacy-analysis-core" % analysisCoreVersion
+
+  private val commonsIOVersion = "2.4"
+  lazy val commonsIO = "commons-io" % "commons-io" % commonsIOVersion
+}
