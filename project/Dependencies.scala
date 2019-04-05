6d482cdb5a298096c50b797d0b2b0ea16a2e73a7
Joao Machado <machado@codacy.com>
2019-04-05 11:01:41 +0100

    feature: Depend on the new codacy-analysis-core
    It now reads codacy.tests.noremove as a property to keep the containers for debug

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,8 +5,9 @@ object Dependencies {
   private val scalatestVersion = "3.0.5"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % scalatestVersion
 
-  private val analysisCoreVersion = "0.1.0-alpha3.4286"
-  lazy val analysisCore = "com.codacy" %% "codacy-analysis-core" % analysisCoreVersion
+  private val analysisCoreVersion = "0.1.0-alpha3.152"
+
+  lazy val analysisCore = "com.codacy" %% "codacy-analysis-core" % analysisCoreVersion  withSources ()
 
   private val commonsIOVersion = "2.4"
   lazy val commonsIO = "commons-io" % "commons-io" % commonsIOVersion
