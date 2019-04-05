3f7ac9c5f8269e4352cb1bb5178a0083f56202cf
Bruno Ferreira <bruno.ferreira@codacy.com>
2019-04-05 13:26:22 +0100

    Bump analysis core

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   private val scalatestVersion = "3.0.5"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % scalatestVersion
 
-  private val analysisCoreVersion = "0.1.0-alpha3.4286"
+  private val analysisCoreVersion = "0.1.0-alpha3.152"
   lazy val analysisCore = "com.codacy" %% "codacy-analysis-core" % analysisCoreVersion
 
   private val commonsIOVersion = "2.4"
