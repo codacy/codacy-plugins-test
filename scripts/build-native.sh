e042cab6d19ca069c49207d58049b9c4662377de
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2019-09-12 09:50:24 +0100

    Fix Native image (#63)
    * Native Image again
    
    * fix: Make native-image build on sbt 1.3.0
    
    * Apply scalafmt

--- scripts/build-native.sh
+++ scripts/build-native.sh
@@ -84,14 +84,16 @@ function build_cmd() {
   local BINARY_NAME=$1
   local APP_MAIN_CLASS=$2
   local APP_CLASSPATH=$3
-  local FLAGS="-O1 -H:+ReportUnsupportedElementsAtRuntime --initialize-at-build-time"
+  local FLAGS="-O1 -J-Xmx4G -H:+ReportExceptionStackTraces --no-fallback --initialize-at-build-time --report-unsupported-elements-at-runtime"
+  # Flags for debug
+  # FLAGS+=" --allow-incomplete-classpath -H:+TraceClassInitialization"
 
   if [[ "${OS_TARGET}" != "darwin" ]]
   then
-    FLAGS="$FLAGS --static"
+    FLAGS+=" --static"
   fi
 
-  echo 'native-image -J-Xmx6G -cp '"${APP_CLASSPATH}"' '"${FLAGS}"' -H:Name='"${BINARY_NAME}"' -H:Class='"${APP_MAIN_CLASS}"
+  echo 'native-image -cp '"${APP_CLASSPATH}"' '"${FLAGS}"' -H:Name='"${BINARY_NAME}"' -H:Class='"${APP_MAIN_CLASS}"
 }
 
 echo "Publishing ${APP_NAME} binary version ${VERSION} for ${OS_TARGET}"
@@ -110,7 +112,7 @@ case "$TARGET" in
       -e JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS}" \
       --user=root \
       --entrypoint=bash \
-      -v $HOME/.ivy2:$HOME/.ivy2 \
+      -v $HOME:$HOME:ro \
       -v $PWD:$PWD \
       findepi/graalvm:19.2.0-all \
         -c 'cd /tmp && '"${BUILD_CMD}"' && mv '"$BINARY_NAME $PWD/$BINARY_NAME"
