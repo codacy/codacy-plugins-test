389203af1349fb8371e831cf646c7e7a38410a16
Luís Ferreira <contact@lsferreira.net>
2019-09-20 18:46:21 +0100

    Revert "Use better files for deleting tmp directories"
    This reverts commit a4b5c082688727b1d5064edf1c36d8fc0ba3ddd7.

--- src/main/scala/codacy/plugins/test/DockerHelpers.scala
+++ src/main/scala/codacy/plugins/test/DockerHelpers.scala
@@ -1,45 +1,41 @@
 package codacy.plugins.test
 
-import scala.sys.process._
-import better.files._
-import java.nio.file.Path
+import java.nio.file.{Files, Path}
+import _root_.scala.sys.process._
 
 object DockerHelpers {
 
   val dockerRunCmd = List("docker", "run", "--net=none", "--privileged=false", "--user=docker")
 
   def testFoldersInDocker(dockerImage: DockerImage): Either[String, Seq[Path]] = {
-    val sourceDirFile = File.newTemporaryDirectory("docker-test-folders")
-    val sourceDir = sourceDirFile.path
+    val sourceDir = Files.createTempDirectory("docker-test-folders")
+
     val dockerStartedCmd = dockerRunCmd ++ List("-d", "--entrypoint=sh", dockerImage.toString)
-    val output = dockerStartedCmd.lineStream_!.headOption
-    try {
-      output match {
-        case Some(containerId) =>
+    val output = dockerStartedCmd.lineStream_!
+
+    output.headOption match {
+      case Some(containerId) =>
+        try {
           //copy files from running container
-          List("docker", "cp", s"$containerId:/docs/directory-tests", sourceDir.toString).!
+          List("docker", "cp", s"$containerId:/docs/directory-tests", sourceDir.toString).lineStream_!.toList
 
           // backwards compatibility, making sure directory tests exist so we can copy the old test dir
-          List("mkdir", "-p", s"$sourceDir/directory-tests").!
-          List("docker", "cp", s"$containerId:/docs/tests", s"$sourceDir/directory-tests").!
+          List("mkdir", "-p", s"$sourceDir/directory-tests").lineStream_!.toList
+          List("docker", "cp", s"$containerId:/docs/tests", s"$sourceDir/directory-tests").lineStream_!.toList
+        } finally {
+          // Remove container
+          List("docker", "rm", "-f", containerId).lineStream_!.toList
+        }
+        val sourcesDir = sourceDir.resolve("directory-tests")
 
-          val sourcesDir = sourceDir.resolve("directory-tests")
+        val pathArr = sourcesDir.toFile.listFiles().collect {
+          case dir if dir.exists() =>
+            dir.toPath
+        }
+        Right(pathArr)
 
-          val pathArr = sourcesDir.toFile.listFiles().collect {
-            case dir if dir.exists() =>
-              dir.toPath
-          }
-          Right(pathArr)
-
-        case None =>
-          Left("[Failure] Couldn't get the container id!")
-      }
-    } finally {
-      for (containerId <- output) {
-        // Remove container
-        List("docker", "rm", "-f", containerId).!
-      }
-      sourceDirFile.delete(swallowIOExceptions = true)
+      case None =>
+        Left("[Failure] Couldn't get the container id!")
     }
   }
 
