0f857be7cc8c9a8c78309b414295033673d80c63
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2019-09-24 11:23:13 +0200

    fix: Fix premature deleting of temp directory

--- src/main/scala/codacy/plugins/test/DockerHelpers.scala
+++ src/main/scala/codacy/plugins/test/DockerHelpers.scala
@@ -1,27 +1,25 @@
 package codacy.plugins.test
 
 import scala.sys.process._
-import better.files._
 import java.nio.file.Path
 
 object DockerHelpers {
+  private val processLogger = ProcessLogger((line: String) => ())
 
   val dockerRunCmd = List("docker", "run", "--net=none", "--privileged=false", "--user=docker")
 
-  def testFoldersInDocker(dockerImage: DockerImage): Either[String, Seq[Path]] = {
-    val sourceDirFile = File.newTemporaryDirectory("docker-test-folders")
-    val sourceDir = sourceDirFile.path
+  def testFoldersInDocker(dockerImage: DockerImage, sourceDir: Path): Either[String, Seq[Path]] = {
     val dockerStartedCmd = dockerRunCmd ++ List("-d", "--entrypoint=sh", dockerImage.toString)
     val output = dockerStartedCmd.lineStream_!.headOption
     try {
       output match {
         case Some(containerId) =>
           //copy files from running container
-          List("docker", "cp", s"$containerId:/docs/directory-tests", sourceDir.toString).!
+          List("docker", "cp", s"$containerId:/docs/directory-tests", sourceDir.toString) ! processLogger
 
           // backwards compatibility, making sure directory tests exist so we can copy the old test dir
-          List("mkdir", "-p", s"$sourceDir/directory-tests").!
-          List("docker", "cp", s"$containerId:/docs/tests", s"$sourceDir/directory-tests").!
+          List("mkdir", "-p", s"$sourceDir/directory-tests") ! processLogger
+          List("docker", "cp", s"$containerId:/docs/tests", s"$sourceDir/directory-tests") ! processLogger
 
           val sourcesDir = sourceDir.resolve("directory-tests")
 
@@ -30,16 +28,14 @@ object DockerHelpers {
               dir.toPath
           }
           Right(pathArr)
-
         case None =>
           Left("[Failure] Couldn't get the container id!")
       }
     } finally {
       for (containerId <- output) {
         // Remove container
-        List("docker", "rm", "-f", containerId).!
+        List("docker", "rm", "-f", containerId) ! processLogger
       }
-      sourceDirFile.delete(swallowIOExceptions = true)
     }
   }
 
