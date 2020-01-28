a3979e6f86471834aacd19127d20050af50cb463
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-01-28 16:13:42 +0000

    test: Add Example Duplication tool test

---
+++ src/test/scala/codacy/plugins/test/ExampleDuplicationToolTests.scala
@@ -0,0 +1,13 @@
+package codacy.plugins.test
+
+import codacy.plugins.DockerTest
+import org.scalatest._
+
+class ExampleDuplicationToolTests extends FunSuite {
+  test("[ExampleDuplicationTool] tests should pass") {
+    DockerTest.main(Array("pattern", "codacy/codacy-example-duplication-tool:latest"))
+  }
+  test("[ExampleDuplicationTool] tests with wrong test name should throw exception") {
+    intercept[Exception](DockerTest.main(Array("patern", "codacy/codacy-example-duplication-tool:latest")))
+  }
+}
