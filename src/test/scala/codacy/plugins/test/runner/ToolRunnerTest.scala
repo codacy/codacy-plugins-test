a281fc46ff2162e600d7a1859365163a26f0448d
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-05-25 17:48:20 +0200

    test: Add test with configuration

--- src/test/scala/codacy/plugins/test/runner/ToolRunnerTest.scala
+++ src/test/scala/codacy/plugins/test/runner/ToolRunnerTest.scala
@@ -11,21 +11,43 @@ import File._
 
 class ToolRunnerTest extends FunSuite {
 
+  private val dockerImage = "codacy/codacy-example-tool:latest"
+
   test("run example tool with no configuration") {
     val file = Paths.get("foobar.txt")
-
     val result: Seq[Result] = (for {
       srcDir <- File.temporaryDirectory(parent = Some(root / "tmp"))
       sourceFile = srcDir / "foobar.txt"
       _ = sourceFile.write("foo")
       config = FileCfg()
-      res = ToolRunner.run("codacy/codacy-example-tool:latest", srcDir, Set(file), config)
+      res = ToolRunner.run(dockerImage, srcDir, Set(file), config)
     } yield res).get()
 
     val expected = Seq(
       Issue(patternId = results.Pattern.Id("foobar"),
             filename = file,
             message = Issue.Message("found foo"),
+            level = results.Result.Level.Info,
+            category = None,
+            location = LineLocation(1))
+    )
+
+    assert(result === expected)
+  }
+
+  test("run example tool with /.codacyrc") {
+    val result: Seq[Result] = (for {
+      srcDir <- File.temporaryDirectory(parent = Some(root / "tmp"))
+      _ = (srcDir / "foobar.txt").write("foo")
+      _ = (srcDir / "test.txt").write("test")
+      config = CodacyCfg(Set(Pattern(id = "foobar", parameters = Set(Parameter(name = "value", value = "test")))))
+      res = ToolRunner.run(dockerImage, srcDir, Set("foobar.txt", "test.txt").map(Paths.get(_)), config)
+    } yield res).get()
+
+    val expected = Seq(
+      Issue(patternId = results.Pattern.Id("foobar"),
+            filename = Paths.get("test.txt"),
+            message = Issue.Message("found test"),
             level = results.Result.Level.Info,
             category = None,
             location = LineLocation(1))
