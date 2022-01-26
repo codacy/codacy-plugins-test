02309fef3e37559389e5be31dae3c2e6e121a114
Luis Ventura <luis.ventura@codacy.com>
2022-01-26 15:41:43 +0000

    fix: Update codacy-duplication-example-tool image

--- src/test/scala/codacy/plugins/test/ExampleDuplicationToolTests.scala
+++ src/test/scala/codacy/plugins/test/ExampleDuplicationToolTests.scala
@@ -5,9 +5,9 @@ import org.scalatest._
 
 class ExampleDuplicationToolTests extends FunSuite {
   test("[ExampleDuplicationTool] tests should pass") {
-    DockerTest.main(Array("pattern", "codacy/codacy-example-duplication-tool:latest"))
+    DockerTest.main(Array("pattern", "codacy/codacy-duplication-example-tool:latest"))
   }
   test("[ExampleDuplicationTool] tests with wrong test name should throw exception") {
-    intercept[Exception](DockerTest.main(Array("patern", "codacy/codacy-example-duplication-tool:latest")))
+    intercept[Exception](DockerTest.main(Array("patern", "codacy/codacy-duplication-example-tool:latest")))
   }
 }
