cf3b313766029d0726d9078179b07570728a8d6b
Vítor Pires <vitor.pires@codacy.com>
2023-12-27 16:26:12 +0000

    add missing categories

--- src/main/scala/codacy/plugins/test/JsonTests.scala
+++ src/main/scala/codacy/plugins/test/JsonTests.scala
@@ -3,6 +3,7 @@ package codacy.plugins.test
 import codacy.utils.CollectionHelper
 import com.codacy.plugins.api.PatternDescription
 import com.codacy.plugins.api.results.Pattern
+import com.codacy.plugins.api.results.Tool
 import com.codacy.plugins.results.traits.DockerToolDocumentation
 import com.codacy.plugins.runners.IDocker
 import com.codacy.plugins.utils.BinaryDockerHelper
@@ -39,70 +40,93 @@ object JsonTests extends ITest {
       }
     }
 
-    (dockerToolDocumentation.toolSpecification, dockerToolDocumentation.patternDescriptions) match {
-      case (Some(tool), Some(descriptions)) =>
-        val diffResult = new CollectionHelper[Pattern.Specification, PatternDescription, String](tool.patterns.toSeq,
-                                                                                                 descriptions.toSeq)({
-          pattern =>
-            val parameters = pattern.parameters.map(_.name.value).toSeq.sorted
-            generateUniquePatternSignature(pattern.patternId.value, parameters)
-        }, { description =>
-          val parameters = description.parameters.getOrElse(Seq.empty).map(_.name.value).toSeq.sorted
-          generateUniquePatternSignature(description.patternId.value, parameters)
-        }).fastDiff
+    val patternCategoryResult = dockerToolDocumentation.toolSpecification.fold(true)(validatePatternCategory)
 
-        val duplicateDescriptions = descriptions.groupBy(_.patternId).filter { case (_, v) => v.size > 1 }
-        if (duplicateDescriptions.nonEmpty) {
-          error(s"""
+    val noMismatchResult =
+      (dockerToolDocumentation.toolSpecification, dockerToolDocumentation.patternDescriptions) match {
+        case (Some(tool), Some(descriptions)) =>
+          val diffResult = new CollectionHelper[Pattern.Specification, PatternDescription, String](tool.patterns.toSeq,
+                                                                                                   descriptions.toSeq)({
+            pattern =>
+              val parameters = pattern.parameters.map(_.name.value).toSeq.sorted
+              generateUniquePatternSignature(pattern.patternId.value, parameters)
+          }, { description =>
+            val parameters = description.parameters.getOrElse(Seq.empty).map(_.name.value).toSeq.sorted
+            generateUniquePatternSignature(description.patternId.value, parameters)
+          }).fastDiff
+
+          val duplicateDescriptions = descriptions.groupBy(_.patternId).filter { case (_, v) => v.size > 1 }
+          if (duplicateDescriptions.nonEmpty) {
+            error(s"""
                |Some patterns were duplicated in /docs/description/description.json
                |
                |  * ${duplicateDescriptions.map { case (patternId, _) => patternId }.mkString(",")}
               """.stripMargin)
-        }
+          }
 
-        if (diffResult.newObjects.nonEmpty) {
-          error(s"""
+          if (diffResult.newObjects.nonEmpty) {
+            error(s"""
                |Some patterns were only found in /docs/patterns.json
                |Confirm that all the patterns and parameters present in /docs/patterns.json are also present in /docs/description/description.json
                |
                |  * ${diffResult.newObjects.map(_.patternId).mkString(",")}
               """.stripMargin)
-        }
+          }
 
-        if (diffResult.deletedObjects.nonEmpty) {
-          error(s"""
+          if (diffResult.deletedObjects.nonEmpty) {
+            error(s"""
                |Some patterns were only found in /docs/description/description.json
                |Confirm that all the patterns and parameters present in /docs/description/description.json are also present in /docs/patterns.json
                |
                |  * ${diffResult.deletedObjects.map(_.patternId).mkString(",")}
               """.stripMargin)
-        }
+          }
 
-        val titlesAboveLimit = descriptions.filter(_.title.length > 255)
-        if (titlesAboveLimit.nonEmpty) {
-          error(s"""
+          val titlesAboveLimit = descriptions.filter(_.title.length > 255)
+          if (titlesAboveLimit.nonEmpty) {
+            error(s"""
                |Some titles are too big in /docs/description/description.json
                |The max size of a title is 255 characters
                |
                | * ${titlesAboveLimit.map(_.patternId).mkString(", ")}
               """.stripMargin)
-        }
+          }
 
-        val descriptionsAboveLimit = descriptions.filter(_.description.getOrElse("").length > 500)
-        if (descriptionsAboveLimit.nonEmpty) {
-          error(s"""
+          val descriptionsAboveLimit = descriptions.filter(_.description.getOrElse("").length > 500)
+          if (descriptionsAboveLimit.nonEmpty) {
+            error(s"""
                |Some descriptions are too big in /docs/description/description.json
                |The max size of a description is 500 characters
                |
                | * ${descriptionsAboveLimit.map(_.patternId).mkString(", ")}
               """.stripMargin)
-        }
+          }
 
-        ignoreDescriptions ||
-        (diffResult.newObjects.isEmpty && diffResult.deletedObjects.isEmpty)
+          ignoreDescriptions ||
+          (diffResult.newObjects.isEmpty && diffResult.deletedObjects.isEmpty)
 
-      case _ => ignoreDescriptions
-    }
+        case _ => ignoreDescriptions
+      }
+
+    patternCategoryResult && noMismatchResult
+  }
+
+  private def validatePatternCategory(toolSpec: Tool.Specification): Boolean = {
+    // TS-682: filter disabled categories
+    val allowedCategories = Pattern.Category.values.filterNot(_ == Pattern.Category.Duplication)
+
+    toolSpec.patterns.forall(pattern => {
+      if (!allowedCategories.contains(pattern.category)) {
+        error(s"""
+           | Invalid pattern (${pattern.patternId}) category (${pattern.category}).
+           | Valid categories are:
+           | ${allowedCategories}
+          """.stripMargin)
+        false
+      } else {
+        true
+      }
+    })
   }
 
   private def readDockerToolDocumentation(dockerImage: DockerImage) = {
